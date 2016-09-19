4829fc9e72637b5d303ec5a88dfa38057db7c5e9
qintianjie <qintianjie@QINTIANJIE.sogou-inc.com>
2016-09-19 13:11:04 +0800

    up

--- src/main/java/com/colorcc/sample/jenkins/web/SampleController.java
+++ src/main/java/com/colorcc/sample/jenkins/web/SampleController.java
@@ -10,7 +10,7 @@ public class SampleController {
 	
 	@RequestMapping(value="/hello", method=RequestMethod.GET)
 	public String hello() {
-		return "Hello world2www.";
+		return "Hello world.";
 	}
 
 }
